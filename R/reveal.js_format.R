b596a1fdd550417e100490decbfe8e0edcaaa81b
Duncan McPherson <duncan@mcpherson.org.uk>
2015-01-30 23:01:22 +0000

    minor tweaks

--- R/reveal.js_format.R
+++ R/reveal.js_format.R
@@ -1,4 +1,4 @@
-#' A rmarkdown format
+#' An rmarkdown format for producing reveal.js slides
 #'
 #' This function allows you to render rmarkdown into reveal.js slide format.
 #' @param toc Do you want a Table of Contents? Defaults to FALSE.
@@ -10,7 +10,7 @@
 #' ---
 #' title: "My Presentation"
 #' output:
-#'   candoo::reveal.js_format:
+#'   Rcandoo::reveal.js_format:
 #'     toc: false
 #' ---
 #' 
@@ -21,15 +21,4 @@ reveal.js_format <- function(toc = FALSE) {
     output_format(knitr = knitr_options(opts_chunk = list(dev = 'png')),
                   pandoc = pandoc_options(to = "html"),
                   clean_supporting = FALSE)
-    # get the locations of resource files located within the package
-    css <- system.file("reports/styles.css", package = "mypackage")
-    header <- system.file("reports/quarterly/header.html", package = "mypackage")
-    
-    # call the base html_document function
-    rmarkdown::html_document(toc = toc,
-                             fig_width = 6.5,
-                             fig_height = 4,
-                             theme = NULL,
-                             css = css,
-                             includes = includes(before_body = header))
 }
